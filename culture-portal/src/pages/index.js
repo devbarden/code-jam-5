a10d125a1cb871688ccbba9263b743a263831216
Heat98 <heeeat98@gmail.com>
2019-01-27 02:58:49 +0300

    #22/ref: refactor HomePage container

--- culture-portal/src/pages/index.js
+++ culture-portal/src/pages/index.js
@@ -1,14 +1,12 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from 'react';
 
-import Layout from '../layouts/Layout'
+import Layout from '../layouts/Layout';
+import MainPage from '../layouts/mainPage/MainPage';
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+   <MainPage/>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
