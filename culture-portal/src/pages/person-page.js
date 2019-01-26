e3ed86b15bd6d020a262cf1481e884267d10a27e
mikhailnakanechny <mikhailnakanechny@gmail.com>
2019-01-26 18:17:14 +0300

    #10 / feat: - add a-vertical-timeline-component-for-react component + add test timeline to person-page

--- culture-portal/src/pages/person-page.js
+++ culture-portal/src/pages/person-page.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { Timeline, TimelineItem } from 'vertical-timeline-component-for-react'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
@@ -14,9 +15,68 @@ const shortInfo = Persons[0].shortInfo
 
 const PersonPage = () => (
   <Layout>
-    <SEO title={name} />
+    <SEO title={name}/>
     <h1>{name}</h1>
-    <p>{name} родился {dateOfBirth} в {placeOfDeath}. {shortInfo}</p>
+    <p>{name} родился {dateOfBirth} в городе {placeOfDeath}. {shortInfo}</p>
+    <Timeline lineColor={'#ddd'}>
+      <TimelineItem
+        key="001"
+        dateText="11/2010 – Present"
+        style={{ color: '#e86971' }}
+      >
+        <h3>Title, Company</h3>
+        <h4>Subtitle</h4>
+        <p>
+                    Est incididunt sint eu minim dolore mollit velit velit commodo ex nulla
+                    exercitation. Veniam velit adipisicing anim excepteur nostrud magna
+                    nostrud aliqua dolor. Sunt aute est duis ut nulla officia irure
+                    reprehenderit laborum fugiat dolore in elit. Adipisicing do qui duis Lorem
+                    est.
+        </p>
+        <p>
+                    Est incididunt sint eu minim dolore mollit velit velit commodo ex nulla
+                    exercitation. Veniam velit adipisicing anim excepteur nostrud magna
+                    nostrud aliqua dolor. Sunt aute est duis ut nulla officia irure
+                    reprehenderit laborum fugiat dolore in elit. Adipisicing do qui duis Lorem
+                    est.
+        </p>
+        <p>
+                    Est incididunt sint eu minim dolore mollit velit velit commodo ex nulla
+                    exercitation. Veniam velit adipisicing anim excepteur nostrud magna
+                    nostrud aliqua dolor. Sunt aute est duis ut nulla officia irure
+                    reprehenderit laborum fugiat dolore in elit. Adipisicing do qui duis Lorem
+                    est.
+        </p>
+      </TimelineItem>
+      <TimelineItem
+        key="002"
+        dateText="04/2009 – 11/2010"
+        dateInnerStyle={{ background: '#61b8ff', color: '#000' }}
+        bodyContainerStyle={{
+          background: '#ddd',
+          padding: '20px',
+          borderRadius: '8px',
+          boxShadow: '0.5rem 0.5rem 2rem 0 rgba(0, 0, 0, 0.2)',
+        }}
+      >
+        <h3 style={{ color: '#61b8ff' }}>Title, Company</h3>
+        <h4 style={{ color: '#61b8ff' }}>Subtitle</h4>
+        <p>
+                    Est incididunt sint eu minim dolore mollit velit velit commodo ex nulla
+                    exercitation. Veniam velit adipisicing anim excepteur nostrud magna
+                    nostrud aliqua dolor. Sunt aute est duis ut nulla officia irure
+                    reprehenderit laborum fugiat dolore in elit. Adipisicing do qui duis Lorem
+                    est.
+        </p>
+        <p>
+                    Est incididunt sint eu minim dolore mollit velit velit commodo ex nulla
+                    exercitation. Veniam velit adipisicing anim excepteur nostrud magna
+                    nostrud aliqua dolor. Sunt aute est duis ut nulla officia irure
+                    reprehenderit laborum fugiat dolore in elit. Adipisicing do qui duis Lorem
+                    est.
+        </p>
+      </TimelineItem>
+    </Timeline>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
