7a749ba86b6206faf632d8c2d061fb2efeb7e63f
Heat98 <heeeat98@gmail.com>
2019-01-26 17:19:35 +0300

    #22/ref: few changes

--- culture-portal/src/pages/page-3.js
+++ culture-portal/src/pages/page-3.js
@@ -2,12 +2,10 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Search from '../components/searchPage/Search'
-import Layout from '../components/layout'
-import SEO from '../components/seo'
+import Layout from '../layouts/layout'
 
 const ThirdPage = () => (
   <Layout>
-    <SEO title="Page three" />
     <Search />
     <Link to="/">Go back to the homepage</Link>
   </Layout>
