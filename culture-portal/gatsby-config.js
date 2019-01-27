36a1d30db6d1f79c9418e891b68346e7f18ebe90
Dzmitry <heeeat98@gmail.com>
2019-01-27 03:12:07 +0300

    Update gatsby-config.js

--- culture-portal/gatsby-config.js
+++ culture-portal/gatsby-config.js
@@ -1,23 +1,15 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${ __dirname }/src/`,
-      },
+    siteMetadata: {
+        title: 'Belarusian cinematography portal',
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    // this (optional) plugin enables Progressive Web App
-    // + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
-  ],
-}
+    plugins: [
+        'gatsby-plugin-react-helmet',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                path: `${__dirname}/src/pages`,
+                name: 'pages',
+            },
+        },
+    ],
+};
